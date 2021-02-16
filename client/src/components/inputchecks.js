6ad45ff669fbf644887de558ae8494ba74bdafae
Mviita65 <mviita65@gmail.com>
2021-02-16 14:46:23 +0200

    tarkistuksia

---
+++ client/src/components/inputchecks.js
@@ -0,0 +1,22 @@
+
+const tarkistaSahkoposti = (email) => {
+    if (email === undefined) {
+        throw new Error('Sähköpostia ei välitetty, tarkista lomake!');
+    }
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
+
+const tarkistaSalasana = (str) => {
+    // at least one number, one lowercase and one uppercase letter
+    // at least six characters
+    if (str === undefined) {
+        throw new Error('Salasanaa ei välitetty, tarkista lomake!');
+    }var res = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+    return res.test(str);
+  }
+
+  export {
+      tarkistaSahkoposti, 
+      tarkistaSalasana
+  }
